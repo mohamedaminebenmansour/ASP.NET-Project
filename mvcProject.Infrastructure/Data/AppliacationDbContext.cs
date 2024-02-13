4c0df6c9ba5af69de9eb6eb35ba9bedcc85de14d
Lenovo <Lenovo@LAPTOP-05617R3V>
2024-02-13 21:18:46 +0100

    create Ville model

---
+++ mvcProject.Infrastructure/Data/AppliacationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace mvcProject.Infrastructure.Data
+{
+    public class AppliacationDbContext : DbContext
+    {
+        //    public AppliacationDbContext(DbContextOptions<AppliacationDbContext> options) : base(options) {
+
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+                                        Initial Catalog = BibliothequeBenFredjSkander; 
+                                        Integrated Security = true");
+        }
+    }
+}
